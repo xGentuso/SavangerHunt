67cf5d87022a59ff37c989c08f03608fda8dbaf4
xGentuso <ryan.mota@triosstudent.com>
2025-02-07 09:51:18 -0500

    Initial Commit

---
+++ SavangerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SavangerHunt
+//
+//  Created by ryan mota on 2025-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SavangerHunt/SavangerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavangerHuntApp.swift
+//  SavangerHunt
+//
+//  Created by ryan mota on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct SavangerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
